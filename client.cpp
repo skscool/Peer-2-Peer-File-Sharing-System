659a4394af012bab2abe189d3020834580d9b65d
Satyam Singh <satyam010singh@gmail.com>
2020-11-01 01:07:45 +0530

    group-based
    - create_group
    - join_group
    - leave_group
    - list_groups
    - list_requests
    - accept_request

--- client.cpp
+++ client.cpp
@@ -51,8 +51,105 @@ int main(int argc, char const *argv[])
     //client send command recieve result in loop
     while(1){
     	char msgToSend[1500] = {0}, buffer[1024] = {0}; 
-	    printf("\n\nEnter a Message : ");							//get command from user
-		gets(msgToSend);
+	    int input_flag = 1;
+	    
+	    while(input_flag){
+	    	memset(&msgToSend[0], 0, sizeof(msgToSend));
+			printf("\n\nEnter a Message : ");							//get command from user
+			gets(msgToSend);
+		
+			char temp_tokenize[1500] = {0};
+			strcpy(temp_tokenize, msgToSend);
+			
+			string cmd(strtok(temp_tokenize, " "));
+			
+			int len = 1;
+			while(strtok(NULL, " ")){
+				len++;
+			}
+			
+			if(cmd == "create_user"){
+				if(len != 3){
+					cout<<"Error : bad arguments! (usage : create_user <user_id> <password>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "login"){
+				if(len != 3){
+					cout<<"Error : bad arguments! (usage : login <user_id> <password>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "logout"){
+				if(len != 1){
+					cout<<"Error : bad arguments! (usage : logout)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "create_group"){
+				if(len != 2){
+					cout<<"Error : bad arguments! (usage : create_group <group_id>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "list_groups"){
+				if(len != 1){
+					cout<<"Error : bad arguments! (usage : list_groups)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "list_requests"){
+				if(len != 2){
+					cout<<"Error : bad arguments! (usage : list_requests <group_id>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "join_group"){
+				if(len != 2){
+					cout<<"Error : bad arguments! (usage : join_group <group_id>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "accept_request"){
+				if(len != 3){
+					cout<<"Error : bad arguments! (usage : accept_request <group_id> <user_id>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "leave_group"){
+				if(len != 2){
+					cout<<"Error : bad arguments! (usage : leave_group <group_id>)"<<endl;
+				}else{
+					input_flag = 0;
+				}
+			}
+			else if(cmd == "bye"){
+				break;
+			}
+			else{
+				cout<<"Error : no such command!"<<endl;
+			}
+			
+			if(cmd!="create_user" && cmd!="login" && strcmp(client_id, "")==0){
+				cout<<"Error : not logged in! can't execute -> " + cmd + " (TIP: login <user_id> <password>)";
+				input_flag = 1;
+			}
+			//cout<<cmd<<" "<<string(client_id)<<" "<<strcmp(client_id, "")<<endl;
+			if(cmd=="login" && strcmp(client_id, "")!=0){
+				cout<<"Error : already logged in to user_id ->" + string(client_id);
+				input_flag = 1;
+			}
+			
+		}
+		
 		strcat(msgToSend, client_id);
 		
 		send(sock , msgToSend , strlen(msgToSend) , 0 ); 			//send command to server
